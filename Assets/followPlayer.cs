f8c1864976575d8cbdfaae36207bab55a43f052c
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2022-11-03 00:39:30 +0200

    Add files via upload

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offSet;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position+offSet;
+    }
+}
